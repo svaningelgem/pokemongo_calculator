55c9b7e3694c1d91385cabe78d5ec7ff3f383d3e
svaningelgem <steven@vaningelgem.be>
2020-12-24 17:39:23 +0100

    Work in progress 2

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+$(document).ready(function () {
+  $("#IV_table").hide();
+  $("#CP_table").hide();
+  $("#Raid_table").hide();
+  $("#Catch_table").hide();
+});
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -5,4 +12,36 @@ function myFunction() {
   } else {
     x.className = "topnav";
   }
+}
+
+$.getJSON("pokemon_information.json", function (data) {
+  let innerHTML = "";
+  $.each(data, function (key, val) {
+    innerHTML +=
+      "<option value='" + val.nice_name + "'>" + val.nice_name + "</option>";
+  });
+  $("#IV_Pokemon")[0].innerHTML = innerHTML;
+  $("#CP_Pokemon")[0].innerHTML = innerHTML;
+  $("#Raid_Pokemon")[0].innerHTML = innerHTML;
+  $("#Catch_Pokemon")[0].innerHTML = innerHTML;
+});
+
+function IVSubmit() {
+  // $("#IV_Pokemon")[0].value
+  // $("#IV_CP")[0].value
+  // $("#IV_HP")[0].value
+  // $("#IV_Stardust")[0].value
+  // $("#IV_Hatched")[0].checked
+  // $("#IV_Lucky")[0].checked
+  $("#IV_table").show(500);
+}
+function CPSubmit() {
+  $("#CP_table").show(500);
+}
+function RaidSubmit() {
+  // if($("#Raid_op1")[0].checked)
+  $("#Raid_table").show(500);
+}
+function CatchSubmit() {
+  $("#Catch_table").show(500);
 }
